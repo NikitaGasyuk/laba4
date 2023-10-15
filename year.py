b9065665e657438eb312602f8e0c24453a5da5b8
NikitaGasyuk <126286242+NikitaGasyuk@users.noreply.github.com>
2023-10-15 17:56:44 +0300

    Update year.py
    вариант 1, 4 лабораторная работа

--- year.py
+++ year.py
@@ -1,9 +1,10 @@
+import datetime
 from random import randint
-def leap_year():
-year = randint(1,3000)
-print(year)
-if year % 4 == 0:
-return True
-else:
-return False
-print(leap_year())
+def is_leap_year(datetime_object):
+    datetime_object = datetime.datetime(randint(1, 3000),1,1)
+    print(datetime_object)
+    if datetime_object.year % 4 == 0:
+        return True
+    else:
+        return False
+print(is_leap_year(datetime_object=()))
